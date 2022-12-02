8b1f5289a5d0fbd607f6f18e86cfc6eed6cf0b2b
hrushikeshhd18 <hrutikdesai1@gmail.com>
2022-12-02 14:40:17 +0530

    initial commit

---
+++ lib/main.dart
@@ -0,0 +1,50 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'bottomnavigation.dart';
+import 'package:responsive_framework/responsive_framework.dart';
+
+
+
+Future<void> main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+
+
+  SystemChrome.setPreferredOrientations(
+      [DeviceOrientation.portraitUp, DeviceOrientation.portraitDown]);
+  runApp(MyApp());
+}
+class MyApp extends StatefulWidget {
+  @override
+  _MyAppState createState()=> new _MyAppState();
+}
+class _MyAppState extends State<MyApp>{
+  @override
+  Widget build(BuildContext context) {
+      return  MaterialApp(
+        builder: (context, widget) => ResponsiveWrapper.builder(
+            BouncingScrollWrapper.builder(context, widget),
+            maxWidth: 1200,
+            minWidth: 450,
+            defaultScale: true,
+            breakpoints: [
+              const ResponsiveBreakpoint.resize(450, name: MOBILE),
+              const ResponsiveBreakpoint.autoScale(800, name: TABLET),
+              const ResponsiveBreakpoint.autoScale(1000, name: TABLET),
+              const ResponsiveBreakpoint.resize(1200, name: DESKTOP),
+              const ResponsiveBreakpoint.autoScale(2460, name: "4K"),
+            ],
+            background: Container(color: Color(0xFFF5F5F5))),
+        debugShowCheckedModeBanner: false,
+        theme: ThemeData(
+            scaffoldBackgroundColor: Colors.white,
+             primarySwatch: Colors.red,
+            textTheme: Theme.of(context).textTheme.apply(bodyColor: Colors.white),
+            visualDensity: VisualDensity.adaptivePlatformDensity,
+          ),
+        home:  BottomNav(),
+
+      );
+  }
+}
+
+
